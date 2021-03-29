aa68f423c5c6e9313f25bf1785a8b2a864a3fce9
Sona Shrestha <sona.shrestha004@gmail.com>
2021-03-29 16:11:50 +1100

    changing function to display local storage

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-var scheduleFormEl1 = $('#form-1');
-var scheduleDataInputEl1 = $('#9');
-var scheduleDataInputEl2 = $('#10');
-var scheduleDataInputEl3 = $('#11');
-var scheduleDataInputEl4 = $('#12');
-var scheduleDataInputEl5 = $('#13');
-var scheduleDataInputEl6 = $('#14');
-var scheduleDataInputEl7 = $('#15');
-var scheduleDataInputEl8 = $('#16');
-var scheduleDataInputEl9 = $('#17');
-var dayActivity = [];
-
 // momentjs : displaying current date
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
@@ -20,31 +8,19 @@ currentHour = moment().format("HH");
 
 // handle schedule form submission
 function handleProjectFormSubmit(event) {
-    event.preventDefault();  
-    console.log($(this).data("target"));
-    var targetData = $(this).data("target"); 
-    // console.log(targetData) ;
-    var inputData = $(`#${targetData}`).val();
-    
-    var scheduleData = inputData.trim(); 
-    //add into array
-    dayActivity.push({hour: targetData, dayActivity: scheduleData});
-    //setting to local storage
-    localStorage.setItem("dayActivity", JSON.stringify(dayActivity)); 
+    event.preventDefault();     
+    var targetData = $(this).data("target");     
+    var inputData = $(`#${targetData}`).val();    
+    var scheduleData = inputData.trim();     
+    localStorage.setItem(targetData, scheduleData); 
 }
 
 //getting stored localstorage
-textBox = JSON.parse(localStorage.getItem("dayActivity")) || [];
-if(textBox.length){
+$(".form-control").each(function() {
+    var hour = $(this).attr('id');
+    $(`#${hour}`).val(localStorage.getItem(hour));
 
-    for (var i = 0; i < textBox.length; i++){
-        // console.log(textBox[i].dayActivity);
-        $(`#${textBox[i].hour}`).val(textBox[i].dayActivity);
-    }
-    // console.log(textBox1);
-    // $('#9').val(textBox1[0].dayActivity);
-}
-
+});
 
 //checking hour compared to current hour
 $(".form-control").each(function() {
@@ -60,10 +36,8 @@ $(".form-control").each(function() {
     
 });
 
-
+//for each form submit
 $('form').each(function(){
     $(this).on('submit', handleProjectFormSubmit);
-
 });
 
-// scheduleFormEl1.on('submit', handleProjectFormSubmit);
